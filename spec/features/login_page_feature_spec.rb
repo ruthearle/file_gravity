b980bd1235baafd1302522c267f42dc4a06d31a5
Ruth Earle <rrearle@gmail.com>
2015-02-26 13:33:00 +0000

    failing test for person filling in login details written

--- spec/features/login_page_feature_spec.rb
+++ spec/features/login_page_feature_spec.rb
@@ -5,4 +5,13 @@ feature 'Login' do
     visit '/'
     expect(page).to have_text 'File Gravity'
   end
+
+  scenario 'A person can fill in login details' do
+    visit '/'
+    fill_in 'Email Address', with: "email@email.com"
+    fill_in 'Password', with: "password"
+    click_on 'Login'
+    expect(current_path).to eq '/file_gravity'
+    expect(page).to have_content 'Welcome'
+  end 
 end
