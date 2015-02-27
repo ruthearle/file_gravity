b25537ffab95bb35ebd24b7d5b873dfbf2b43014
Ruth Earle <rrearle@gmail.com>
2015-02-27 17:02:13 +0000

    Test for a person logging in with correct details - passed.

--- server.rb
+++ server.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-require 'rest-client'
+require './lib/work_client'
 
 enable :sessions
 
@@ -10,6 +10,12 @@ get '/' do
 end
 
 post '/session' do
-  email, password = params['email', 'password']
-  RestClient.get 'http://my.workshare.com/'
+  @email, @password = params['email'], params['password']
+  response = WorkClient.new.login(@email, @password)
+  session[:response] = response['my_session_id']
+  redirect to '/files'
+end
+
+get '/files' do
+  erb :files
 end
