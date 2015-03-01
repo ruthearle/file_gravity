c7e2933bd0adbed52fd989a58c2c27eaab3ab06d
Ruth Earle <rrearle@gmail.com>
2015-03-01 12:30:03 +0000

    failing test for listing file written

--- spec/files_spec.rb
+++ spec/files_spec.rb
@@ -3,8 +3,18 @@ require 'files'
 
 describe 'Files' do
 
-  it "#initialize" do
-    files_list = [{'id'=>'1'}]
-    expect{Files.new(files_list)}.not_to raise_error
+  describe "#initialize" do
+    it "will not raise an error when initialised" do
+      files_list = [{'id'=>'1'}]
+      expect{Files.new(files_list)}.not_to raise_error
+    end
+  end
+
+  describe "#files" do
+    it "lists files with their name and file extension" do
+      file = "music.mp3"
+      size = "1mb"
+      expect(Files.new(file).files).to eq "#{file}.#{size}"
+    end
   end
 end
