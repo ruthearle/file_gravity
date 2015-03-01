4a40a245160b7649896924ad6108e83209f475bc
Ruth Earle <rrearle@gmail.com>
2015-03-01 13:53:10 +0000

    failing test written for calculating file gravity

--- spec/files_spec.rb
+++ spec/files_spec.rb
@@ -3,7 +3,7 @@ require 'files'
 
 describe 'Files' do
 
-  let(:files_list)  { [{'id'=>'1'}] }
+  let(:files_list)  { [{'id'=>'1', 'name'=>'document', 'extension'=>'docx', 'size'=>'0.001003'}] }
   let(:files) { Files.new(files_list) }
 
   describe "#initialize" do
@@ -14,14 +14,15 @@ describe 'Files' do
 
   describe "#list" do
     it "lists files with their name and file extension" do
-      file = [{'name' => 'music', 'extension' => 'mp3', 'size' => '0.001003'}]
-      expect(Files.new(file).list).to eq ["type" => "music.mp3", "size" => "0.001003"]
+      #file = [{'name' => 'music', 'extension' => 'mp3', 'size' => '0.001003'}]
+      expect(Files.new(files_list).list).to eq ["type" => "document.docx", "size" => "0.001003"]
     end
   end
 
   describe "#calculate_gravity" do
     it "calculates the basic weight of a file" do
-
+      weight = 0.0014042
+      expect(files.calculate_gravity).to eq weight
     end
   end
 end
