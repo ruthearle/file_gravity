dd8a722c0348b435909beec0acfe03e8d0b9030e
Ruth Earle <rrearle@gmail.com>
2015-03-01 13:41:27 +0000

    Refactored tests

--- spec/files_spec.rb
+++ spec/files_spec.rb
@@ -3,10 +3,12 @@ require 'files'
 
 describe 'Files' do
 
+  let(:files_list)  { [{'id'=>'1'}] }
+  let(:files) { Files.new(files_list) }
+
   describe "#initialize" do
     it "will not raise an error when initialised" do
-      files_list = [{'id'=>'1'}]
-      expect{Files.new(files_list)}.not_to raise_error
+      expect{files}.not_to raise_error
     end
   end
 
@@ -14,6 +16,11 @@ describe 'Files' do
     it "lists files with their name and file extension" do
       file = [{'name' => 'music', 'extension' => 'mp3', 'size' => '0.001003'}]
       expect(Files.new(file).list).to eq ["type" => "music.mp3", "size" => "0.001003"]
+    end
+  end
+
+  describe "#calculate_gravity" do
+    it "calculates the basic weight of a file" do
 
     end
   end
