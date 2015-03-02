3c41d04efd8305c6e2c62c5197e6bca8166dd5fa
Ruth Earle <rrearle@gmail.com>
2015-03-02 15:21:02 +0000

    Refactored test. Now testing for #files. failing

--- lib/file_meta.rb
+++ lib/file_meta.rb
@@ -1,15 +1,20 @@
 class FileMeta
 
-  attr_accessor :metadata, :file_types
-    FILE_TYPES = YAML::load_file('file_types.yml')
+  attr_accessor :metadata
+  CATEGORIES =  {"document" => {"extension" => ['doc', 'docx', 'pages', 'odt', 'rtf'], "gravity" => 1.1}, 
+                 "song"     => {"extension" => ['mp3', 'wma', 'wav', 'm4a'], "gravity" => 1.2 },
+                 "video"    => {"extension" => ['avi', 'mp4', 'mpg', 'mov', 'wmv', 'flv'], "gravity" => 1.4 },
+                 "text"     => {"extension" => ['txt'], "gravity" => 100},
+                 "other"    => {"extension" => [], "gravity" => 1.0}
+                }
 
-  def initialize(files)
-    self.metadata = files.to_a
+  def initialize(list)
+    self.metadata = list
   end
 
   def list
     formatted_files_list = []
-    metadata.each { |file| formatted_files_list << {'type' => "#{file['name']}.#{file['extension']}", 'size' => "#{file['size']}"}}
+    metadata.each { |file| formatted_files_list << {'name' => "#{file['name']}.#{file['extension']}", 'size' => "#{file['size']}"}}
     formatted_files_list
     #list = formatted_files_list.map { |file|  p "#{file['type']} #{(file['size'].to_f / 1000000.0).round(10)}"}
     #list
--- spec/file_meta_spec.rb
+++ spec/file_meta_spec.rb
@@ -10,16 +10,32 @@ describe 'FileMeta' do
                         "size"=>16377897,
                         "created_at"=>"2015-02-26T09:57:42Z",
                         "updated_at"=>"2015-02-26T09:58:08Z",
-                        "creator"=>{
-                          "name"=>"Ruth Earle",
+                        "creator"=>
+                         {"name"=>"Ruth Earle",
                           "email"=>"me@ruthearle.com",
                           "uuid"=>"71f72a76-1c9c-4961-ba3c-2c59a43142b1"
-                        },
+                         },
                         "url"=>"https://my.workshare.com/decks/14913224",
                         "access_token"=>"Q1_lGRAy0bx90M6v",
                         "first_page_url"=>"https://my.workshare.com/images/icons/medium/mp3.png",
-                        "folder_id"=>1209741}]
-                    }
+                        "folder_id"=>1209741
+                        },
+                        {"id"=>14913212,
+                            "name"=>"binary",
+                            "extension"=>"bin",
+                            "version"=>1,
+                            "size"=>1003,
+                            "created_at"=>"2015-02-26T09:57:26Z",
+                            "updated_at"=>"2015-02-26T09:57:28Z",
+                            "creator"=>
+                             {"name"=>"Ruth Earle",
+                              "email"=>"me@ruthearle.com",
+                              "uuid"=>"71f72a76-1c9c-4961-ba3c-2c59a43142b1"},
+                            "url"=>"https://my.workshare.com/decks/14913212",
+                            "access_token"=>"jDHB9OiKpjCEX25Z",
+                            "first_page_url"=>"https://my.workshare.com/images/icons/medium/unknown.png",
+                            "folder_id"=>1168672}
+                      ]}
   let(:files) { FileMeta.new(files_list) }
 
   describe "#initialize" do
@@ -28,10 +44,9 @@ describe 'FileMeta' do
     end
   end
 
-  describe "#list" do
-    it "lists files with their name and file extension" do
-      #file = [{'name' => 'music', 'extension' => 'mp3', 'size' => '0.001003'}]
-      expect(FileMeta.new(files_list).list).to eq ["type" => "music.mp3", "size"=>"16377897"]
+  describe "#files" do
+    it "lists all files" do
+      expect(FileMeta.new(files_list).files).to eq files_list
     end
   end
 
