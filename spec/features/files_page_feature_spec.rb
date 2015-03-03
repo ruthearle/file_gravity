cb10077f20f5ba5207a11e0bfdbfaf7c55933a74
Ruth Earle <rrearle@gmail.com>
2015-03-03 12:54:56 +0000

    refactored files_page spec to use login helper

--- spec/features/files_page_feature_spec.rb
+++ spec/features/files_page_feature_spec.rb
@@ -1,34 +1,31 @@
 require 'spec_helper'
+require_relative '../helpers/login_helper'
+
+include LoginHelper
 
 feature "/files" do
 
-  scenario "displaying file category names" do
+  before(:each) do
+    login("me@ruthearle.com","makersWelcome")
+  end
 
-    visit '/'
-    fill_in 'Email Address', with: "placement@makersacademy.com"
-    fill_in 'Password', with: "makersWelcome"
-    click_on 'Login'
+  scenario "displaying file category names" do
     expect(current_path).to eq '/files'
     expect(page).to have_content 'Video'
   end
 
   scenario "display number of files per category" do
-    visit '/'
-    fill_in 'Email Address', with: "me@ruthearle.com"
-    fill_in 'Password', with: "makersWelcome"
-    click_on 'Login'
     expect(current_path).to eq '/files'
-    expect(page).to have_content 'Video'
-    expect(page).to have_content '1'
+    expect(page).to have_content 'Video 1'
   end
 
   scenario "display basic weight of files per category" do
-    visit '/'
-    fill_in 'Email Address', with: "me@ruthearle.com"
-    fill_in 'Password', with: "makersWelcome"
-    click_on 'Login'
     expect(current_path).to eq '/files'
     expect(page).to have_content 'Video 1 3.12'
+  end
+
+  scenario "shows the total weight for all files" do
+
   end
 
 
