40427b3d49580f48891b946c54da4709c532e64a
Ruth Earle <rrearle@gmail.com>
2015-03-04 09:28:00 +0000

    failing test for incorrect credentials

--- spec/features/homepage_feature_spec.rb
+++ spec/features/homepage_feature_spec.rb
@@ -16,4 +16,14 @@ feature '/' do
     expect(current_path).to eq '/'
     expect(page).to have_content "Thank you for using File Gravity, goodbye."
   end
+
+  scenario 'A person cannot login with incorrect credentials' do
+    visit '/'
+    fill_in 'email', with: 'email@email.com'
+    fill_in 'password', with: 'password'
+    click_on 'Login'
+    expect(current_path).to eq '/'
+    expect(page).to have_content 'Invalid email or password. Pleaase try again.'
+  end
+
 end
